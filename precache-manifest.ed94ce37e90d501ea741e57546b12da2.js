1b27a24c3234f9cfcd9a92da63f3bd44ba83cbcc
iparhai <iparhai@nu.edu.pk>
2021-11-22 14:20:28 +0500

    Updates

---
+++ precache-manifest.ed94ce37e90d501ea741e57546b12da2.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "78712902fa07c83047c9",
+    "url": "/Subtraction_WordProblems/static/css/main.69c498fe.chunk.css"
+  },
+  {
+    "revision": "78712902fa07c83047c9",
+    "url": "/Subtraction_WordProblems/static/js/main.9e71e512.chunk.js"
+  },
+  {
+    "revision": "435f03015b193558651c",
+    "url": "/Subtraction_WordProblems/static/js/runtime~main.7ad8681a.js"
+  },
+  {
+    "revision": "a4d0e11acf243ff31913",
+    "url": "/Subtraction_WordProblems/static/js/2.c82c1172.chunk.js"
+  },
+  {
+    "revision": "f0aa0efad6746287de4c",
+    "url": "/Subtraction_WordProblems/static/js/3.d25448f0.chunk.js"
+  },
+  {
+    "revision": "6fc126f248e4b520c28a6229ca641bc0",
+    "url": "/Subtraction_WordProblems/static/media/mango.6fc126f2.png"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/Subtraction_WordProblems/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "821cde131dca119a05d6f45ed5115aad",
+    "url": "/Subtraction_WordProblems/static/media/grocery.821cde13.gif"
+  },
+  {
+    "revision": "1e695820a1572347f6a0a76f53280775",
+    "url": "/Subtraction_WordProblems/static/media/tostartpressbutton.1e695820.mp3"
+  },
+  {
+    "revision": "b8fca396c389eea881872cc7f0e7855b",
+    "url": "/Subtraction_WordProblems/static/media/321go.b8fca396.mp3"
+  },
+  {
+    "revision": "88e8a851b1954501951ae45cf46c1e2f",
+    "url": "/Subtraction_WordProblems/static/media/rightAnswer.88e8a851.mp3"
+  },
+  {
+    "revision": "48b32efb6257ce0ee9f40b87b5894871",
+    "url": "/Subtraction_WordProblems/static/media/wrongAnswer.48b32efb.mp3"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/Subtraction_WordProblems/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/Subtraction_WordProblems/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/Subtraction_WordProblems/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/Subtraction_WordProblems/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/Subtraction_WordProblems/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/Subtraction_WordProblems/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/Subtraction_WordProblems/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/Subtraction_WordProblems/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/Subtraction_WordProblems/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/Subtraction_WordProblems/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/Subtraction_WordProblems/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "a0cc197d7b1c5ac20f1a5b0a60cc45ac",
+    "url": "/Subtraction_WordProblems/static/media/typing.a0cc197d.mp3"
+  },
+  {
+    "revision": "d625354e0fe3750134af83e0e4faefd4",
+    "url": "/Subtraction_WordProblems/static/media/apple.d625354e.png"
+  },
+  {
+    "revision": "411c4e6a0a9e83696dc8f57a70f342c0",
+    "url": "/Subtraction_WordProblems/static/media/banana.411c4e6a.png"
+  },
+  {
+    "revision": "091d77928921a2ccefc1456f02198651",
+    "url": "/Subtraction_WordProblems/static/media/orange.091d7792.png"
+  },
+  {
+    "revision": "da95b1d736ed03b78a44d44ef15d813a",
+    "url": "/Subtraction_WordProblems/static/media/pear.da95b1d7.png"
+  },
+  {
+    "revision": "73fe00995cd7adebd696e8d1715500dc",
+    "url": "/Subtraction_WordProblems/static/media/pineapple.73fe0099.png"
+  },
+  {
+    "revision": "5690694919b062e5d201f2895ca093b7",
+    "url": "/Subtraction_WordProblems/static/media/plum.56906949.png"
+  },
+  {
+    "revision": "7b19bf7bd224cdd9941921ee087b2bdd",
+    "url": "/Subtraction_WordProblems/static/media/pomegranate.7b19bf7b.png"
+  },
+  {
+    "revision": "2684d075fc2c72e38524ffc4f7d5df41",
+    "url": "/Subtraction_WordProblems/static/media/strawberry.2684d075.png"
+  },
+  {
+    "revision": "d1d59a88467a6b381df3eee8ce160f25",
+    "url": "/Subtraction_WordProblems/static/media/watermelon.d1d59a88.png"
+  },
+  {
+    "revision": "18ba7a065467efa32ebef20844f445f9",
+    "url": "/Subtraction_WordProblems/static/media/lemon.18ba7a06.png"
+  },
+  {
+    "revision": "7498fd0032f151294c930c93367bd0f9",
+    "url": "/Subtraction_WordProblems/static/media/carrot.7498fd00.png"
+  },
+  {
+    "revision": "560c5f81cbe99a953264d06e0c7cd014",
+    "url": "/Subtraction_WordProblems/static/media/cherry.560c5f81.png"
+  },
+  {
+    "revision": "baee8b37557cf9ba7e6c7016c19d3b06",
+    "url": "/Subtraction_WordProblems/static/media/peach.baee8b37.png"
+  },
+  {
+    "revision": "8506c659726847019116c0159642b02c",
+    "url": "/Subtraction_WordProblems/static/media/dragon_fruit.8506c659.png"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/Subtraction_WordProblems/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "18c8ae84a367ae39c7dc07370d5e133e",
+    "url": "/Subtraction_WordProblems/static/media/paperBag.18c8ae84.png"
+  },
+  {
+    "revision": "e036ffe49c3290b294f2edb25b336a5d",
+    "url": "/Subtraction_WordProblems/static/media/grapes.e036ffe4.png"
+  },
+  {
+    "revision": "f5f258693cd37ccdf82ff06568ee9082",
+    "url": "/Subtraction_WordProblems/static/media/background.f5f25869.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/Subtraction_WordProblems/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/Subtraction_WordProblems/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/Subtraction_WordProblems/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "be949893b6914dc313d945e51ac98c72",
+    "url": "/Subtraction_WordProblems/index.html"
+  }
+];
